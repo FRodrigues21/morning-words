94a22ad62afe66714f4f58fe6515ff58470941ea
Francisco Rodrigues <frodrigues@opensoft.pt>
2019-07-30 13:52:23 +0100

    Updates

---
+++ precache-manifest.85399e4a6bd3d27bde517558249598b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16af67347d1735e6bc01e877f0c888de",
+    "url": "/morning-words/index.html"
+  },
+  {
+    "revision": "ed90eeaa0bc15acf3d6a",
+    "url": "/morning-words/static/js/2.f79170f5.chunk.js"
+  },
+  {
+    "revision": "21fdef1a9620c9fddde9",
+    "url": "/morning-words/static/js/main.ac0e0f05.chunk.js"
+  },
+  {
+    "revision": "892f9924d39a0904a94f",
+    "url": "/morning-words/static/js/runtime~main.e04febc5.js"
+  }
+]);
